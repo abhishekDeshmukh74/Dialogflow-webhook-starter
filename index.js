1523e1655249f9d26febd37f6a91a7fb1993c109
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2017-10-05 18:02:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,50 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     speech = speech+" location Details"
-    return res.json({
-        speech: speech,
-        displayText: speech,
+    
+    var options = {
+        url: 'https://rezility-dev.herokuapp.com/api/getnearbyproperties',
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNTlhZmM1OTJmYTIyNjcwMDExYmMyY2I4IiwidXNlcl9lbWFpbCI6InNoZWhuaWxhQG1haWxpbmF0b3IuY29uIiwiaWF0IjoxNTA0NjkxNjAyfQ.KLH0fuaNWFVOAvJA9shjONcsMr68Pwhp35Z78tboEho',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "user_id": "59afc592fa22670011bc2cb8",
+            "latitude": 18.548056,
+            "longitude": 73.9004693,
+            "is_housing_property_needed": true,
+            "service_interests": [
+                "5859382a4cce7614b8b0bd66_23",
+                "5859382a4cce7614b8b0bd66_24",
+                "5859382a4cce7614b8b0bd66_26",
+                "5859382a4cce7614b8b0bd66_25",
+                "5859382a4cce7614b8b0bd66_27",
+                "5859382a4cce7614b8b0bd66_28",
+                "5859382a4cce7614b8b0bd66_29",
+                "5859382a4cce7614b8b0bd66_30"
+            ],
+            "app_id": "22062016",
+            "app_version": "1.2.0",
+            "device_id": "web-dev1",
+            "device_type": "android",
+            "sys_user_id": "59afc592fa22670011bc2cb8"
+        })
+    }
+
+    request(options, function(error, response, body){
+    if(!error && response.statusCode == 200) {
+        return res.json({
+        speech: response.body.message,
+        displayText: response.body.message,
         source: 'webhook-echo-sample'
-    });
+    });     
+    } else {
+        return res.json({
+            speech: response.body.message,
+            displayText: response.body.message,
+            source: 'webhook-echo-sample'
+    }
 });
 
 restService.listen((process.env.PORT || 8000), function() {
