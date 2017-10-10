aec8a7db2b6b074c9d6f56cd550cf037bcf184b6
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2017-10-10 15:11:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,12 +18,13 @@ restService.post('/', function(req, res) {
 
     var temp = '/'+req.body.result.action;
     console.log(req.body);
-    
+
     if(temp!=undefined){
         console.log("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL");
         console.log(temp);
 
-        restService.post(temp,function(req,res){
+        if(temp=="rezpolls/getfeeds"){
+            //restService.post(temp,function(req,res){
             console.log("success");
             var options = {
                 url: 'https://rezility-dev.herokuapp.com/api/rezpolls/getfeeds',
@@ -60,53 +61,57 @@ restService.post('/', function(req, res) {
 
             });	
 
-        });
-    }
+            // });
 
-
-    var options = {
-        url: 'https://rezility-dev.herokuapp.com/api/getnearbyproperties',
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNTlhZmM1OTJmYTIyNjcwMDExYmMyY2I4IiwidXNlcl9lbWFpbCI6InNoZWhuaWxhQG1haWxpbmF0b3IuY29uIiwiaWF0IjoxNTA0NjkxNjAyfQ.KLH0fuaNWFVOAvJA9shjONcsMr68Pwhp35Z78tboEho',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "user_id": "59afc592fa22670011bc2cb8",
-            "latitude": 18.548056,
-            "longitude": 73.9004693,
-            "is_housing_property_needed": true,
-            "service_interests": [
-                "5859382a4cce7614b8b0bd66_23",
-                "5859382a4cce7614b8b0bd66_24",
-                "5859382a4cce7614b8b0bd66_26",
-                "5859382a4cce7614b8b0bd66_25",
-                "5859382a4cce7614b8b0bd66_27",
-                "5859382a4cce7614b8b0bd66_28",
-                "5859382a4cce7614b8b0bd66_29",
-                "5859382a4cce7614b8b0bd66_30"
-            ],
-            "app_id": "22062016",
-            "app_version": "1.2.0",
-            "device_id": "web-dev1",
-            "device_type": "android",
-            "sys_user_id": "59afc592fa22670011bc2cb8"
-        })
-    }
-
-    request(options, function(error, response, body){
-        if(!error && response.statusCode == 200) {
-            var serviceData = JSON.parse(body);
-            var speech = serviceData.service_providers[0].public_name+" is available at address "+serviceData.service_providers[0].street_address1;
-            return res.json({
-                speech: speech,
-                displayText: speech,
-                source: 'webhook-echo-sample'
-            });     
-        } else {
-            console.log("in else");
         }
-    });
+
+        if(temp=="getnearbyproperties"){
+            var options = {
+                url: 'https://rezility-dev.herokuapp.com/api/getnearbyproperties',
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNTlhZmM1OTJmYTIyNjcwMDExYmMyY2I4IiwidXNlcl9lbWFpbCI6InNoZWhuaWxhQG1haWxpbmF0b3IuY29uIiwiaWF0IjoxNTA0NjkxNjAyfQ.KLH0fuaNWFVOAvJA9shjONcsMr68Pwhp35Z78tboEho',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "user_id": "59afc592fa22670011bc2cb8",
+                    "latitude": 18.548056,
+                    "longitude": 73.9004693,
+                    "is_housing_property_needed": true,
+                    "service_interests": [
+                        "5859382a4cce7614b8b0bd66_23",
+                        "5859382a4cce7614b8b0bd66_24",
+                        "5859382a4cce7614b8b0bd66_26",
+                        "5859382a4cce7614b8b0bd66_25",
+                        "5859382a4cce7614b8b0bd66_27",
+                        "5859382a4cce7614b8b0bd66_28",
+                        "5859382a4cce7614b8b0bd66_29",
+                        "5859382a4cce7614b8b0bd66_30"
+                    ],
+                    "app_id": "22062016",
+                    "app_version": "1.2.0",
+                    "device_id": "web-dev1",
+                    "device_type": "android",
+                    "sys_user_id": "59afc592fa22670011bc2cb8"
+                })
+            }
+
+            request(options, function(error, response, body){
+                if(!error && response.statusCode == 200) {
+                    var serviceData = JSON.parse(body);
+                    var speech = serviceData.service_providers[0].public_name+" is available at address "+serviceData.service_providers[0].street_address1;
+                    return res.json({
+                        speech: speech,
+                        displayText: speech,
+                        source: 'webhook-echo-sample'
+                    });     
+                } else {
+                    console.log("in else");
+                }
+            });
+
+        }
+    }
 
 });
 
