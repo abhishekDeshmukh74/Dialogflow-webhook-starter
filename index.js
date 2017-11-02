07bad8d6810e75880c6f414813ebd8a275090ce3
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2017-11-02 19:16:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ restService.post('/', function(req, res) {
     // var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     // speech = speech+" location Details"
     var tokenAnduserid = req.body.originalRequest.data.accessToken;
-    console.log(accessToken);
+    console.log(tokenAnduserid);
     var resultArray = tokenAnduserid.split('-');
     authorizationToken = resultArray[0]
     userId = resultArray[1]
