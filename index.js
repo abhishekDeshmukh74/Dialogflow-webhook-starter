2baf2730adb23363881efef15592349088102b91
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2017-11-02 16:41:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,8 @@ restService.use(bodyParser.json());
 restService.post('/', function(req, res) {
     // var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     // speech = speech+" location Details"
-    console.log(req.originalRequest.data);            
+    console.log(req);            
     var temp = req.body.result.action;
-    console.log(req.body);
 
     if(temp!=undefined){
         console.log(temp);
